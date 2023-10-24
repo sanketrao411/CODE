00b988779c2d787f928160a7291c6e2a9d29d57c
SANKET SHINDE <129519283+sanketrao411@users.noreply.github.com>
2023-10-25 00:15:29 +0530

    created array.cpp

---
+++ Array/array.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ 
+    int arr[5];
+
+    //taking input from array
+    
+    for (int i = 0; i < 5; i++)
+    {
+        cin >> arr[i];
+    }
+    
+    //printing array
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
+    return 0;
+}
